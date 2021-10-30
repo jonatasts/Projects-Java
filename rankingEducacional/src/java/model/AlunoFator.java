d9647f58ef15966755e2b6f97d75d29888163468
jonatasts <jonatassantos96@hotmail.com>
2021-10-30 20:59:32 -0300

    👔 Refactor AlunoFator model

--- rankingEducacional/src/java/model/AlunoFator.java
+++ rankingEducacional/src/java/model/AlunoFator.java
@@ -2,66 +2,33 @@ package model;
 
 public class AlunoFator {
 
-    private String matriculaAluno;
-    private int anoEmCurso;
-    private int anoLetivo;
-    private int[] fatores = new int[30];
-    private String observacao;
+  private String matriculaAluno;
+  private String fatorId;
+  private boolean resposta;
 
-    public AlunoFator() {
-    }
+  public AlunoFator() {}
 
-    public String getMatriculaAluno() {
-        return matriculaAluno;
-    }
+  public String getMatriculaAluno() {
+    return matriculaAluno;
+  }
 
-    public void setMatriculaAluno(String matriculaAluno) {
-        this.matriculaAluno = matriculaAluno;
-    }
+  public void setMatriculaAluno(String matriculaAluno) {
+    this.matriculaAluno = matriculaAluno;
+  }
 
-    public int getAnoEmCurso() {
-        return anoEmCurso;
-    }
+  public String getFatorId() {
+    return fatorId;
+  }
 
-    public void setAnoEmCurso(int anoEmCurso) {
-        this.anoEmCurso = anoEmCurso;
-    }
+  public void setFatorId(String fatorId) {
+    this.fatorId = fatorId;
+  }
 
-    public int getAnoLetivo() {
-        return anoLetivo;
-    }
+  public boolean getResposta() {
+    return resposta;
+  }
 
-    public void setAnoLetivo(int anoLetivo) {
-        this.anoLetivo = anoLetivo;
-    }
-
-    public int getFatores(int posicao) {
-        return fatores[posicao];
-    }
-    
-    public int[] getFatores() {
-        return fatores;
-    }
-    
-    public void setFatores(int posicao, int fator) {
-        this.fatores[posicao] = fator;
-    }
-    
-    public void setFatores(int[] fatores) {
-        this.fatores = fatores;
-    }
-
-    public int getTamanhoFatores() {
-        return this.fatores.length;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-    
-    
+  public void setResposta(boolean resposta) {
+    this.resposta = resposta;
+  }
 }
