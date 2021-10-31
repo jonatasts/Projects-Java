2c20c2ea931f57d906fc7a193999e5638a275963
jonatasts <jonatassantos96@hotmail.com>
2021-10-30 22:14:23 -0300

    👔 Refactor alunoFatorDAO class

--- rankingEducacional/src/java/dao/AlunoFatorDAO.java
+++ rankingEducacional/src/java/dao/AlunoFatorDAO.java
@@ -10,157 +10,126 @@ import java.util.ArrayList;
 import model.AlunoFator;
 
 public class AlunoFatorDAO {
-    
-    private IDatabase banco = IDatabase.createDatabase();
-    private ResultSet resultSet;
-    private PreparedStatement preparedStatement;
-    private Statement statement;
-    private Connection conexao;
 
-    public AlunoFatorDAO() throws SQLException {
-        try {
-            conexao = banco.getConnection();
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro conexão com banco em AlunoFatorDAO: " + erro);
-        }
+  private IDatabase banco = IDatabase.createDatabase();
+  private ResultSet resultSet;
+  private PreparedStatement preparedStatement;
+  private Statement statement;
+  private Connection conexao;
+
+  public AlunoFatorDAO() throws SQLException {
+    try {
+      conexao = banco.getConnection();
+    } catch (RuntimeException erro) {
+      throw new RuntimeException(
+        "Erro conexão com banco em AlunoFatorDAO: " + erro
+      );
     }
-    
-     public void insert(AlunoFator alunoFator) throws SQLException {
-        String insertAluno = "INSERT INTO alunofator (matriculaaluno, anoemCurso, anoletivo, observacao, pf1, pf2, pf3 , pf4, "
-                           + "pf5, pf6, pf7, pf8, pf9, pf10, pf11, pf12, pf13, pf14, pf15, pf16, pf17, pf18, pf19, "
-                           + "pf20, pf21, pf22, pf23, pf24, pf25, pf26, pf27, pf28, pf29, pf30) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-       
-        preparedStatement = conexao.prepareStatement(insertAluno);
+  }
 
-        try {
-            preparedStatement.setString(1, alunoFator.getMatriculaAluno());
-            preparedStatement.setInt(2, alunoFator.getAnoEmCurso());
-            preparedStatement.setInt(3, alunoFator.getAnoLetivo());
-            preparedStatement.setString(4,alunoFator.getObservacao());
-            
-            for (int i = 0; i < 30; i++) {
-                 preparedStatement.setInt(i+5,alunoFator.getFatores(i));
-            }
-            
-            
-            preparedStatement.execute();
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro insert: " + erro);
-        }
+  public void insert(AlunoFator alunoFator) throws SQLException {
+    String insertAluno =
+      "INSERT INTO aluno_fator (aluno_id, fator_id, resposta) VALUES (?,?,?)";
 
+    preparedStatement = conexao.prepareStatement(insertAluno);
+
+    try {
+      preparedStatement.setString(1, alunoFator.getMatriculaAluno());
+      preparedStatement.setString(2, alunoFator.getFatorId());
+      preparedStatement.setInt(3, alunoFator.getResposta());
+
+      preparedStatement.execute();
+    } catch (RuntimeException erro) {
+      throw new RuntimeException("Erro insert: " + erro);
     }
+  }
 
-    public ArrayList<AlunoFator> select() throws SQLException {
-        String sql = "SELECT * FROM alunoFator";
-        ArrayList<AlunoFator> alunoFatores = new ArrayList<>();
+  public ArrayList<AlunoFator> select() throws SQLException {
+    String sql = "SELECT * FROM aluno_fator";
+    ArrayList<AlunoFator> alunoFatores = new ArrayList<>();
 
-        try {
-            preparedStatement = conexao.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
-            
-            while (resultSet.next()) {
-                AlunoFator alunoFator = new AlunoFator();
+    try {
+      preparedStatement = conexao.prepareStatement(sql);
+      resultSet = preparedStatement.executeQuery();
 
-                alunoFator.setMatriculaAluno(resultSet.getString("matriculaaluno"));
-                alunoFator.setAnoEmCurso(resultSet.getInt("anoEmCurso"));
-                alunoFator.setAnoLetivo(resultSet.getInt("anoLetivo"));
-                alunoFator.setObservacao(resultSet.getString("observacao"));
-                
-                for (int i = 0; i < 30; i++) {
-                 alunoFator.setFatores(i, (resultSet.getInt(i+5)));
-                }
-                alunoFatores.add(alunoFator);
-            }
-
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro select: " + erro);
-        }
-
-        resultSet.close();
-        conexao.close();
-        return alunoFatores;
-    }
-    
-    public AlunoFator selectAlunoFatorByMatricula(String matricula) throws SQLException {
-        String sql = "SELECT * FROM alunofator WHERE matriculaaluno = ?";
-        int pontuacao = 0;
+      while (resultSet.next()) {
         AlunoFator alunoFator = new AlunoFator();
 
-        try {
-            preparedStatement = conexao.prepareStatement(sql);
-            preparedStatement.setString(1, matricula);
-            resultSet = preparedStatement.executeQuery();
-            
+        alunoFator.setMatriculaAluno(resultSet.getString("aluno_id"));
+        alunoFator.setFatorId(resultSet.getString("fator_id"));
+        alunoFator.setResposta(resultSet.getInt("resposta"));
 
-            if(resultSet.next()) {
-                
-
-                alunoFator.setMatriculaAluno(resultSet.getString("matriculaaluno"));
-                alunoFator.setAnoEmCurso(resultSet.getInt("anoEmCurso"));
-                alunoFator.setAnoLetivo(resultSet.getInt("anoLetivo"));
-                alunoFator.setObservacao(resultSet.getString("observacao"));
-                
-                for (int i = 0; i < 30; i++) {
-                 alunoFator.setFatores(i, (resultSet.getInt(i+5)));
-                }
-            }
-
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro select by matricula: " + erro);
-        }
-
-        //resultSet.close();
-        conexao.close();
-        return alunoFator;
+        alunoFatores.add(alunoFator);
+      }
+    } catch (RuntimeException erro) {
+      throw new RuntimeException("Erro select: " + erro);
     }
-    
-    public void update(AlunoFator alunoFator) throws SQLException {
-        String updateAluno = "UPDATE alunofator SET anoemCurso = ?, anoletivo = ?, observacao = ?, pf1 = ?, pf2 = ?, pf3 = ? , pf4 = ?, "
-                           + "pf5 = ?, pf6 = ?, pf7 = ?, pf8 = ?, pf9 = ?, pf10 = ?, pf11 = ?, pf12 = ?, pf13 = ?, pf14 = ?, pf15 = ?, pf16 = ?, pf17 = ?, pf18 = ?, pf19 = ?, "
-                           + "pf20 = ?, pf21 = ?, pf22 = ?, pf23 = ?, pf24 = ?, pf25 = ?, pf26 = ?, pf27 = ?, pf28 = ?, pf29 = ?, pf30 = ? WHERE matriculaaluno = ?";
-       
-        preparedStatement = conexao.prepareStatement(updateAluno);
 
-        try {
-            
-            preparedStatement.setInt(1, alunoFator.getAnoEmCurso());
-            preparedStatement.setInt(2, alunoFator.getAnoLetivo());
-            preparedStatement.setString(3,alunoFator.getObservacao());
-            
-            for (int i = 0; i < 30; i++) {
-                 preparedStatement.setInt(i+4,alunoFator.getFatores(i));
-            }
-            
-            preparedStatement.setString(34, alunoFator.getMatriculaAluno());
-            
-            
-            preparedStatement.execute();
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro update: " + erro);
-        }
+    resultSet.close();
+    conexao.close();
+    return alunoFatores;
+  }
+
+  public AlunoFator selectAlunoFatorByMatricula(String matricula)
+    throws SQLException {
+    String sql = "SELECT * FROM aluno_fator WHERE aluno_id = ?";
+    int pontuacao = 0;
+    AlunoFator alunoFator = new AlunoFator();
+
+    try {
+      preparedStatement = conexao.prepareStatement(sql);
+      preparedStatement.setString(1, matricula);
+      resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+        alunoFator.setMatriculaAluno(resultSet.getString("aluno_id"));
+        alunoFator.setFatorId(resultSet.getString("fator_id"));
+        alunoFator.setResposta(resultSet.getInt("resposta"));
+      }
+    } catch (RuntimeException erro) {
+      throw new RuntimeException("Erro select by matricula: " + erro);
     }
-    
-    public void delete(String matricula) throws SQLException {
-        String deleteAlunoRanking = "DELETE FROM ranking WHERE idaluno = ?";
-        String deleteAlunoFator = "DELETE FROM alunofator WHERE matriculaaluno = ?";
-        PreparedStatement preparedStmt;
-        
-        try {
-            preparedStmt = conexao.prepareStatement(deleteAlunoRanking);
-            preparedStmt.setString(1,matricula);
-            preparedStmt.execute();
-            
-            preparedStatement = conexao.prepareStatement(deleteAlunoFator);
-            preparedStatement.setString(1,matricula);
-            preparedStatement.execute();
-            
-            
-            conexao.close();
-        } catch (RuntimeException erro) {
-            throw new RuntimeException("Erro Delete: " + erro);
-        }
-        
-        
+
+    //resultSet.close();
+    conexao.close();
+    return alunoFator;
+  }
+
+  public void update(AlunoFator alunoFator) throws SQLException {
+    String updateAluno =
+      "UPDATE aluno_fator SET fator_id = ?, resposta = ? WHERE aluno_id = ?";
+
+    preparedStatement = conexao.prepareStatement(updateAluno);
+
+    try {
+      preparedStatement.setString(1, alunoFator.getFatorId());
+      preparedStatement.setInt(2, alunoFator.getResposta());
+
+      preparedStatement.setString(3, alunoFator.getMatriculaAluno());
+
+      preparedStatement.execute();
+    } catch (RuntimeException erro) {
+      throw new RuntimeException("Erro update: " + erro);
     }
-    
+  }
+
+  public void delete(String matricula) throws SQLException {
+    String deleteAlunoRanking = "DELETE FROM ranking WHERE id_aluno_fator = ?";
+    String deleteAlunoFator = "DELETE FROM aluno_fator WHERE aluno_id = ?";
+    PreparedStatement preparedStmt;
+
+    try {
+      preparedStmt = conexao.prepareStatement(deleteAlunoRanking);
+      preparedStmt.setString(1, matricula);
+      preparedStmt.execute();
+
+      preparedStatement = conexao.prepareStatement(deleteAlunoFator);
+      preparedStatement.setString(1, matricula);
+      preparedStatement.execute();
+
+      conexao.close();
+    } catch (RuntimeException erro) {
+      throw new RuntimeException("Erro Delete: " + erro);
+    }
+  }
 }
