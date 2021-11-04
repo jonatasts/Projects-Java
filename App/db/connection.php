0b11fe80e88f42557b1c243431de1dcdd5055060
jonatasts <jonatassantos96@hotmail.com>
2021-11-03 22:26:58 -0300

    ♻️ Refactor variable name

--- App/db/connection.php
+++ App/db/connection.php
@@ -1,5 +1,5 @@
 <?php
 //Conexão com o Banco de Dados de Monitum
 
-$conexao = pg_pconnect("host=localhost port=5432 dbname=monitum user=postgres password=postgres") or die('Não foi possível conectar ao Banco do Monitum!');
-pg_query($conexao, "set client_encoding to 'UTF8'");
+$connection = pg_pconnect("host=localhost port=5432 dbname=monitum user=postgres password=postgres") or die('Não foi possível conectar ao Banco do Monitum!');
+pg_query($connection, "set client_encoding to 'UTF8'");
