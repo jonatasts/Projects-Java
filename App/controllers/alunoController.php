9c0736fa331435635771b30a5c6b9b1e28abcee0
jonatasts <jonatassantos96@hotmail.com>
2021-11-06 10:54:11 -0300

    🔨 Create select method on alunoController

--- App/controllers/alunoController.php
+++ App/controllers/alunoController.php
@@ -22,12 +22,43 @@ class AlunoController
         }
     }
 
-    public function select($aluno)
+    public function selectAllAlunos()
     {
+        try {
+
+            $query = "SELECT * FROM aluno;";
+
+            pg_query($this->connection, $query);
+        } catch (Exception $e) {
+            //echo $e->getMessage();
+            return false;
+        }
     }
 
     public function selectAlunoByMatricula($matricula)
     {
+        try {
+            $aluno = new Aluno();
+            $query = "SELECT * FROM aluno WHERE matricula = '$matricula';";
+
+            $result = pg_query($this->connection, $query);
+
+            if ($result) {
+                $row = pg_fetch_assoc($result);
+
+                $aluno->setMatriculaAluno($row['matricula']);
+                $aluno->setSerieEmCurso($row['serie_em_curso']);
+                $aluno->setAnoLetivo($row['ano_letivo']);
+                $aluno->setObservacao($row['observacao']);
+
+                return $aluno;
+            }
+
+            return null;
+        } catch (Exception $e) {
+            //echo $e->getMessage();
+            return false;
+        }
     }
 
     public function update()
